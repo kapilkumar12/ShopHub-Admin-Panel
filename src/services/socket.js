d4a1538b1d69817c987a5ac5f1fb6b10a61bc0c7
kapilkumar12 <kapilkumar1492003@gmail.com>
2026-05-04 15:18:40 +0530

    .

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://shophub-backend-5fs0.onrender.com/", {
+const socket = io("https://shophub-backend-5fs0.onrender.com", {
   withCredentials: true,
 });
 
