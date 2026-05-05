21ae99d0520b08dc262f21e262bc57c03df80cd9
kapilkumar12 <kapilkumar1492003@gmail.com>
2026-05-05 10:48:57 +0530

    ..

--- src/services/api.js
+++ src/services/api.js
@@ -5,13 +5,4 @@ const API = axios.create({
   withCredentials: true,
 });
 
-API.interceptors.request.use((config) => {
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
-
 export default API;
